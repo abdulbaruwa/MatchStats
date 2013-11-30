772d0e91a13006f956bb6990041636e919605f8f
abdul baruwa <abdulbaruwa@hotmail.com>
2013-11-30 17:38:42 +0000

    Include Akavache Package and SQLite vsix for binaries

--- MatchStats/Model/ScorePoint.cs
+++ MatchStats/Model/ScorePoint.cs
@@ -1,40 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 
 namespace MatchStats.Model
 {
     public class ScorePoint
     {
         public string Name { get; set; }
+        public string DisplayName { get; set; }
         public PointReason PointReason { get; set; }
+        public Player Player { get; set; }
+    }
+
+    public class ScorPoint
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsForcedError { get; set; }
         public Player Player { get; set; }
     }
 
     public enum PointReason
     {
-        //[Display9"Forehand Winner"]
         ForeHandWinner,
         BackHandWinner,
+        VolleyWinner,
+        DropShotWinner,
+        OverheadWinner,
+        UnforcedForeHand,
+        UnforcedForehandError,
+        UnforcedBackhadError,
+        ForcedError
+    }
 
+    public enum PointLossReason
+    {
+        UnforcedForehandError,
+        UnforcedBackhadError,
+        ForcedError
+    }
+
+    public class BreakPoint
+    {
+        public Player Player { get; set; }
+        public bool Won { get; set; }
     }
 
     public class GameScoreEngine
     {
-        
+        private IList<BreakPoint> _breakPoints;
         private IList<ScorePoint> _playerOnePoints;
         private IList<ScorePoint> _playerTwoPoints;
 
         public GameScoreEngine()
         {
+            _breakPoints = new List<BreakPoint>();
             _playerOnePoints = new List<ScorePoint>();
             _playerTwoPoints = new List<ScorePoint>();
         }
-
+        
         public void PointScored(ScorePoint scorePoint)
         {
             //if(ScorePoint.Player)
         }
-        
     }
 }
