1b5a3b10331ed307e81557a950140f3cbb244f12
Shangoman <abdulbaruwa@hotmail.com>
2014-01-13 09:23:01 +0000

    Update ActionsViewModelTests.cs
    Action Ace Command Tests

--- MatchStats.Test/ViewModels/MatchScoreViewModelTests/ActionsViewModelTests.cs
+++ MatchStats.Test/ViewModels/MatchScoreViewModelTests/ActionsViewModelTests.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Xml.Linq;
-using Akavache;
-using MatchStats.Enums;
-using MatchStats.Model;
-using MatchStats.ViewModels;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-using ReactiveUI;
 
 namespace MatchStats.Test.ViewModels.MatchScoreViewModelTests
 {
@@ -20,10 +12,108 @@ namespace MatchStats.Test.ViewModels.MatchScoreViewModelTests
         {
             var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
             fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+            Assert.IsTrue(fixture.PlayerOneActions.Any(x => x.Name == "AceServe" && x.IsEnabled));
+        }
+
+        [TestMethod]
+        public void ShouldEnableAceActionCommandWhenAPlayerTwoIsServing()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerTwoAsCurrentServerCommand.Execute(null);
+            Assert.IsTrue(fixture.PlayerTwoActions.Any(x => x.Name == "AceServe" && x.IsEnabled));
+        }
+
+        [TestMethod]
+        public void ShouldDisableAceActionCommandWhenPlayerOneServesFirstServeIn()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
 
             fixture.PlayerOneFirstServeInCommand.Execute(null);
 
-            Assert.IsTrue(fixture.PlayerOneActions.Any(x => x.Name == "AceServe" && x.IsEnabled));
+            Assert.IsFalse(fixture.PlayerOneActions.First(x => x.Name == "AceServe").IsEnabled);
+        }
+
+        [TestMethod]
+        public void ShouldDisableAceActionCommandWhenPlayerTwoServesFirstServeIn()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerTwoAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeInCommand.Execute(null);
+
+            Assert.IsFalse(fixture.PlayerTwoActions.First(x => x.Name == "AceServe").IsEnabled);
+        }
+
+        [TestMethod]
+        public void ShouldEnableAceActionCommandWhenPlayerOneServesTheFirstServeOut()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+
+            Assert.IsTrue(fixture.PlayerOneActions.First(x => x.Name == "AceServe").IsEnabled);
+        }
+
+        [TestMethod]
+        public void ShouldEnableAceActionCommandWhenScoreIsGamePointAndPlayerOneServesTheFirstServeOut()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeInCommand.Execute(null);
+            fixture.PlayerTwoActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeInCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeInCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeInCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+
+            Assert.IsTrue(fixture.PlayerOneActions.First(x => x.Name == "AceServe").IsEnabled);
+        }
+
+        [TestMethod]
+        public void ShouldEnableAceActionCommandWhenPlayerTwoServesTheFirstServeOut()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerTwoAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerTwoFirstServeOutCommand.Execute(null);
+
+            Assert.IsTrue(fixture.PlayerTwoActions.First(x => x.Name == "AceServe").IsEnabled);
+        }
+
+
+        [TestMethod]
+        public void ShouldDisableAceActionCommandWhenPlayerOneServesSecondServeIn()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneSecondServeInCommand.Execute(null);
+
+            Assert.IsFalse(fixture.PlayerOneActions.First(x => x.Name == "AceServe").IsEnabled);
+        }
+
+
+        [TestMethod]
+        public void ShouldDisableAceActionCommandWhenPlayerTwoServesSecondServeIn()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerTwoAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerTwoFirstServeOutCommand.Execute(null);
+            fixture.PlayerTwoSecondServeInCommand.Execute(null);
+
+            Assert.IsFalse(fixture.PlayerTwoActions.First(x => x.Name == "AceServe").IsEnabled);
         }
     }
 }
