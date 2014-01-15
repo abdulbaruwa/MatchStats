d20c4ec26ff59e6ea5e8a11fd9a10639e7a5f489
Shangoman <abdulbaruwa@hotmail.com>
2014-01-15 13:33:18 +0000

    Update DoubleFaultCommandViewModel.cs
    Add UndoLastAction command will refactor this out later

--- MatchStats/ViewModels/DoubleFaultCommandViewModel.cs
+++ MatchStats/ViewModels/DoubleFaultCommandViewModel.cs
@@ -1,10 +1,60 @@
-﻿using System;
+using System;
 using System.Linq;
+using System.Windows.Input;
 using MatchStats.Model;
 using ReactiveUI;
 
 namespace MatchStats.ViewModels
 {
+    public class UndoLastActionCommandViewModel : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Match currentMatch = null;
+            var matchStatsApi = RxApp.DependencyResolver.GetService<IMatchStatsApi>();
+
+            //Can this be passed in when the command is called?
+            matchStatsApi.GetCurrentMatch().Subscribe(x => currentMatch = x);
+
+            Game currentGame = null;
+            var currentSet = currentMatch.Score.Sets.FirstOrDefault(x => x.IsCurrentSet);
+            if (currentSet != null)
+            {
+                currentGame = currentSet.Games.FirstOrDefault(x => x.IsCurrentGame);
+            }
+
+            var continueUndo = true;
+            while (continueUndo)
+            {
+                if (currentMatch.MatchStats.LastOrDefault() == null) break;
+                var lastMatchStat = currentMatch.MatchStats.Last();
+                continueUndo = lastMatchStat.UndoPrevious;
+                if (lastMatchStat.PointWonLostOrNone != PointWonLostOrNone.NotAPoint)
+                {
+                    if (lastMatchStat.Player.IsPlayerOne) currentMatch.CurrentGame().PlayerOneScore--;
+                    if (! lastMatchStat.Player.IsPlayerOne) currentMatch.CurrentGame().PlayerTwoScore--;
+
+                    if (currentMatch.CurrentGame().PlayerOneScore < 0 || currentMatch.CurrentGame().PlayerTwoScore < 0) //Less than zero will indicate we have should have undo unto previous game
+                    {
+                        var games = currentMatch.CurrentSet().Games.Count;
+                        currentMatch.CurrentSet().Games.RemoveAt(games - 1);
+                    }
+                }
+                currentMatch.MatchStats.RemoveAt(currentMatch.MatchStats.Count - 1);
+            }
+
+            currentMatch = matchStatsApi.ApplyGameRules(currentMatch);
+            matchStatsApi.SaveMatch(currentMatch);
+            MessageBus.Current.SendMessage(currentMatch, "PointUpdateForCurrentMatch");
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
     public class DoubleFaultCommandViewModel : ReactiveObject, IGameActionViewModel
     {
         public DoubleFaultCommandViewModel(Player player = null)
