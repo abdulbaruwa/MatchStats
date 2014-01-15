617960567d11c43ce28b42148e14208bc4193a86
Shangoman <abdulbaruwa@hotmail.com>
2014-01-15 13:37:11 +0000

    Update UndoActionsViewModel.cs

--- MatchStats.Test/ViewModels/MatchScoreViewModelTests/UndoActionsViewModel.cs
+++ MatchStats.Test/ViewModels/MatchScoreViewModelTests/UndoActionsViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using MatchStats.Model;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
@@ -47,5 +47,34 @@ namespace MatchStats.Test.ViewModels.MatchScoreViewModelTests
             Assert.AreEqual(StatDescription.FirstServeIn, fixture.CurrMatch.MatchStats.Last().Reason);
         }
 
+        [TestMethod]
+        public void WhenLastPointActionIsUnDoneShouldReflectChangeInTheScore()
+        {
+
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+
+            // Two ace serves, the score is 30-0 to player One
+            fixture.PlayerOneActions.First(x => x.Name == "AceServe").ActionCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "AceServe").ActionCommand.Execute(null);
+
+            //Act
+            fixture.UndoLastActionCommand.Execute(null);
+
+            Assert.AreEqual("15", fixture.CurrMatch.GetPlayerOneCurrentScore());
+        }
+
+        [TestMethod]
+        public void ShouldReverseGameWonWhenWinningPointForAGameIsReversed()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+
+            MatchScoreViewModelTestHelper.AddAGameForPlayerOne(fixture);
+
+            fixture.UndoLastActionCommand.Execute(null);
+
+            Assert.AreEqual(1, fixture.CurrMatch.Score.Sets.First().Games.Count);
+        }
     }
 }
