3956754680d68460d6417b5f84208c4e5f553069
Shangoman <shangoman@outlook.com>
2014-01-29 19:02:34 +0000

    staged

--- MatchStats.Test/MatchStatsTests.cs
+++ MatchStats.Test/MatchStatsTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using MatchStats.Model;
@@ -16,7 +18,40 @@ namespace MatchStats.Test
         public async Task ShouldCalculatePercentageFirstServesInForPlayerOne()
         {
             var matchStats = await SerializeMatchStatsFromJsonData();
+            var matchstat = new MatchStat();
+            var player1 = new Player();
+            var player2 = new Player();
+            var matchStatsList = new List<MatchStat>();
+            //15-0
+            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn});
+
+            //15-15
+            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeOut});
+            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.SecondServeIn});
+
+            //30-15
+            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn});
+            //30-30
+            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn});
+            //40-30
+            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.PointWon, Reason = StatDescription.FirstServeAce});
+            //40-40
+            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeOut });
+            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.PointLost, Reason = StatDescription.DoubleFault });
+            //Adv
+            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn });
+            //Game
+            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeOut });
+            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.SecondServeIn });
+
+            var match = new Match {MatchStats = matchStatsList};
             
+            var firstServes = matchStatsList.Where(x => x.Reason == StatDescription.FirstServeIn
+                && x.Reason == StatDescription.FirstServeOut && x.Reason == StatDescription.FirstServeAce).ToList();
+            var firstServesIn = firstServes.Where(x => x.Reason == StatDescription.FirstServeAce && x.Reason == StatDescription.FirstServeIn).ToList();
+            var percentageFirstServer = firstServesIn.Count()/firstServes.Count()*100;
+
+
         }
 
         private static async Task<List<Match>> SerializeMatchStatsFromJsonData()
