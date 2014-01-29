19b603bb01ae03050422ec432c7eb8600e64af60
Shangoman <abdulbaruwa@hotmail.com>
2014-01-29 08:58:24 +0000

    Update MatchStatsTests.cs

--- MatchStats.Test/MatchStatsTests.cs
+++ MatchStats.Test/MatchStatsTests.cs
@@ -1,15 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using MatchStats.Model;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MatchStats.Test
 {
@@ -18,17 +15,23 @@ namespace MatchStats.Test
         [TestMethod]
         public async Task ShouldCalculatePercentageFirstServesInForPlayerOne()
         {
+            var matchStats = await SerializeMatchStatsFromJsonData();
+            
+        }
+
+        private static async Task<List<Match>> SerializeMatchStatsFromJsonData()
+        {
             var path = Windows.ApplicationModel.Package.Current.InstalledLocation.Path + @"\TestData\MyMatchStats.txt";
             var storageFile = await Windows.Storage.StorageFile.GetFileFromPathAsync(path);
             var buffer = await FileIO.ReadBufferAsync(storageFile);
-
-            var ret = default(dynamic);
+            var ret = "";
             using (var datareader = DataReader.FromBuffer(buffer))
             {
                 ret = datareader.ReadString(buffer.Length);
             }
-            ret = JsonConvert.DeserializeObject(ret);
 
+            var data = JObject.Parse(ret)["Value"];
+            return JsonConvert.DeserializeObject<List<Match>>(data.ToString());
         }
     }
 }
