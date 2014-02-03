e1c059068efe7930d42a8a00f5c559e0c9acf67f
Shangoman <abdulbaruwa@hotmail.com>
2014-02-03 08:53:10 +0000

    Update MatchScoreViewModelTests2.cs

--- MatchStats.Test/ViewModels/MatchScoreViewModelTests/MatchScoreViewModelTests2.cs
+++ MatchStats.Test/ViewModels/MatchScoreViewModelTests/MatchScoreViewModelTests2.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using MatchStats.Model;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
@@ -21,7 +21,72 @@ namespace MatchStats.Test.ViewModels.MatchScoreViewModelTests
             Assert.IsNotNull(fixture.CurrMatch.MatchStats.LastOrDefault());
             Assert.IsTrue(fixture.CurrMatch.MatchStats.LastOrDefault().MatchSituations.Count > 0);
             Assert.AreEqual(MatchSituationType.GamePoint, fixture.CurrMatch.MatchStats.Last().MatchSituations.Last().MatchSituationType);
+        }        
+        
+        [TestMethod]
+        public void ShouldAddBreakPointSituationToMatchStatOnBreakPoint()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
 
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "DoubleFault").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "DoubleFault").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "DoubleFault").ActionCommand.Execute(null);
+
+            Assert.IsNotNull(fixture.CurrMatch.MatchStats.LastOrDefault());
+            Assert.IsTrue(fixture.CurrMatch.MatchStats.LastOrDefault().MatchSituations.Count > 0);
+            Assert.AreEqual(MatchSituationType.BreakPoint, fixture.CurrMatch.MatchStats.Last().MatchSituations.Last().MatchSituationType);
         }
+
+         
+        [TestMethod]
+        public void ShouldAddGamePointWonSituationToMatchStatOnGamePointWon()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+            fixture.PlayerOneFirstServeInCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "ForeHandWinner").ActionCommand.Execute(null);
+
+            Assert.IsNotNull(fixture.CurrMatch.MatchStats.LastOrDefault());
+            Assert.IsTrue(fixture.CurrMatch.MatchStats.LastOrDefault().MatchSituations.Count > 0);
+            Assert.AreEqual(MatchSituationType.GamePointWon, fixture.CurrMatch.MatchStats.Last().MatchSituations.Last().MatchSituationType);
+        }
+
+        [TestMethod]
+        public void ShouldAddBreakPointWonSituationToMatchStatOnBreakPointWon()
+        {
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture();
+
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "DoubleFault").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "DoubleFault").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "DoubleFault").ActionCommand.Execute(null);
+
+            fixture.PlayerOneFirstServeOutCommand.Execute(null);
+            fixture.PlayerOneActions.First(x => x.Name == "DoubleFault").ActionCommand.Execute(null);
+
+            Assert.IsNotNull(fixture.CurrMatch.MatchStats.LastOrDefault());
+            Assert.IsTrue(fixture.CurrMatch.MatchStats.LastOrDefault().MatchSituations.Count > 0);
+            Assert.AreEqual(MatchSituationType.BreakPointWon, fixture.CurrMatch.MatchStats.Last().MatchSituations.Last().MatchSituationType);
+        }
+
+
+        
     }
 }
