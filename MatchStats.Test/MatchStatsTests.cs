262742c209e0c322f2b920a099bcae4f982672d3
Shangoman <abdulbaruwa@hotmail.com>
2014-02-14 10:27:38 +0000

    Update MatchStatsTests.cs
    Implement Stats calculation based on new Match schema

--- MatchStats.Test/MatchStatsTests.cs
+++ MatchStats.Test/MatchStatsTests.cs
@@ -32,67 +32,32 @@ namespace MatchStats.Test
 
             var fixtureStats = new MatchStatsViewModel();
             fixtureStats.CurrentMatch = fixture.CurrMatch;
-            Assert.AreEqual("62%", fixtureStats.Stats.First(x => x.StatNameType == StatName.FirstServePercentage).ForMatchP1);
-
-
-            var firstServes = from s in fixture.CurrMatch.Sets
-                from g in s.Games
-                from p in g.Points
-                from sr in p.Serves
-                where sr.IsFirstServe
-                select sr;
-
+            Assert.AreEqual("67%", fixtureStats.Stats.First(x => x.StatNameType == StatName.FirstServePercentage).ForMatchP1);
+            Assert.AreEqual("2", fixtureStats.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForMatchP1);
+            Assert.AreEqual("0", fixtureStats.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForMatchP2);
+            Assert.AreEqual("2", fixtureStats.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForFirstSetP1);
+            Assert.AreEqual("0", fixtureStats.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForFirstSetP2);
         }
+
 
         [TestMethod]
-        public async Task ShouldCalculatePercentageFirstServesInAndDoubleFaultsForPlayerOne()
+        public void ShouldCalculateWinPercentForFirstServeForPlayerOne()
         {
-            var matchs = await SerializeMatchStatsFromJsonData();
+            var fixture = MatchScoreViewModelTestHelper.CreateNewMatchFixture(FinalSetFormats.TenPointChampionShipTieBreak);
+            fixture.SetPlayerOneAsCurrentServerCommand.Execute(null);
 
-            var matchstat = new MatchStat();
-            var player1 = new PlayerBuilder().WithFirstNameSecondName("Ade", "Wilson").IsPlayerOne(true).Build();
-            var player2 = new PlayerBuilder().WithFirstNameSecondName("Luke", "Watson").IsPlayerOne(true).Build();
-            var setId = matchs.First().Sets.First().SetId;
-            var matchStatsList = new List<MatchStat>();
-            //15-0
-            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn, SetId = setId});
+            MatchScoreViewModelTestHelper.PlayerFirstServeOutAndDoubleFault(fixture, true);
+            MatchScoreViewModelTestHelper.PlayerSecondServeInAndLostPoint(fixture, true);
+            MatchScoreViewModelTestHelper.PlayerFirstServeInAndLostPoint(fixture, true);
+            MatchScoreViewModelTestHelper.PlayerFirstServedAndForehandWinner(fixture, true);
+            MatchScoreViewModelTestHelper.PlayerFirstServedAndForehandWinner(fixture, true);
 
-            //15-15
-            matchStatsList.Add(new MatchStat(){Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeOut, SetId = setId});
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.SecondServeIn, SetId = setId });
+            var fixtureStats = new MatchStatsViewModel();
+            fixtureStats.CurrentMatch = fixture.CurrMatch;
+            Assert.AreEqual("50%", fixtureStats.Stats.First(x => x.StatNameType == StatName.WinPercentForFirstServe).ForMatchP1);
 
-            //30-15
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn, SetId = setId });
-            //30-30
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn, SetId = setId });
-            //40-30
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.PointWon, Reason = StatDescription.FirstServeAce, SetId = setId });
-            //40-40
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeOut, SetId = setId });
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.PointLost, Reason = StatDescription.DoubleFault, SetId = setId });
-            //Adv
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeIn, SetId = setId });
-            //Game
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.FirstServeOut, SetId = setId });
-            matchStatsList.Add(new MatchStat() { Player = player1, PointWonLostOrNone = PointWonLostOrNone.NotAPoint, Reason = StatDescription.SecondServeIn, SetId = setId });
-
-            //var match = new Match {MatchStats = matchStatsList, PlayerOne = player1, PlayerTwo = player2};
-            var match = matchs.FirstOrDefault();
-
-            match.MatchStats.Clear();
-            match.MatchStats.AddRange(matchStatsList);
-            var fixture = new MatchStatsViewModel();
-            fixture.CurrentMatch = match;
-            Assert.IsNotNull(fixture.Stats);
-            Assert.IsTrue(fixture.Stats.Count > 0);
-            Assert.AreEqual("62%", fixture.Stats.First(x => x.StatNameType == StatName.FirstServePercentage).ForMatchP1);
-            Assert.IsNotNull(fixture.Stats.FirstOrDefault(x => x.StatNameType == StatName.DoubleFaults));
-            Assert.AreEqual("1", fixture.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForMatchP1);
-            Assert.AreEqual("0", fixture.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForMatchP2);
-            Assert.AreEqual("1", fixture.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForFirstSetP1);
-            Assert.AreEqual("0", fixture.Stats.First(x => x.StatNameType == StatName.DoubleFaults).ForFirstSetP2);
         }
-        
+
 
         private static async Task<List<Match>> SerializeMatchStatsFromJsonData()
         {
