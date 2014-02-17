a746ddf903922146ae36634c0ceda46f3df50ee8
shangoman <abdulbaruwa@hotmail.com>
2014-02-17 22:11:28 +0000

    Minor ui fix

--- MatchStats/Views/MatchScoreView.xaml.cs
+++ MatchStats/Views/MatchScoreView.xaml.cs
@@ -27,8 +27,7 @@ namespace MatchStats.Views
             this.Bind(ViewModel, x => x.ShowHidePopup, x => x.AddMatchPoupup.IsOpen);
             this.Bind(ViewModel, x => x.PlayerOneIsServing, x => x.PlayerOneIsServing.IsChecked);
             this.Bind(ViewModel, x => x.PlayerTwoIsServing, x => x.PlayerTwoIsServing.IsChecked);
-            this.Bind(ViewModel, x => x.MatchStatus, x => x.MatchStatus.Text);
-            this.Bind(ViewModel, x => x.CurrMatch.Tournament.TournamentName , x => x.TournamentName.Text);
+            //  this.Bind(ViewModel, x => x.CurrMatch.Tournament.TournamentName , x => x.TournamentName.Text);
             this.BindCommand(ViewModel, x => x.SetPlayerOneAsCurrentServerCommand, x => x.PlayerOneIsServing);
             this.BindCommand(ViewModel, x => x.SetPlayerTwoAsCurrentServerCommand, x => x.PlayerTwoIsServing);
             this.BindCommand(ViewModel, x => x.PlayerOneFirstServeInCommand, x => x.PlayerOneFirstServe);
