8deaaf11473512a4a518a37c1dfbea48305aeac0
Shangoman <abdulbaruwa@hotmail.com>
2014-03-11 08:52:56 +0000

    Update UserProfileViewModel.cs

--- MatchStats/ViewModels/UserProfileViewModel.cs
+++ MatchStats/ViewModels/UserProfileViewModel.cs
@@ -1,6 +1,5 @@
+using System;
 using System.Runtime.Serialization;
-using Windows.UI.Xaml;
-using MatchStats.Controls;
 using ReactiveUI;
 
 namespace MatchStats.ViewModels
@@ -8,6 +7,26 @@ namespace MatchStats.ViewModels
     [DataContract]
     public class UserProfileViewModel : ReactiveObject
     {
+        public IReactiveCommand NavAwayCommand { get; protected set; }
+
+        public UserProfileViewModel()
+        {
+            NavAwayCommand = new ReactiveCommand();
+            NavAwayCommand.Subscribe(_ => CloseSelf());
+        }
+
+        private void CloseSelf()
+        {
+            ShowMe = false;
+        }
+
+        [DataMember]
+        private bool _showMe;
+        public bool ShowMe
+        {
+            get { return _showMe; }
+            set { this.RaiseAndSetIfChanged(ref _showMe, value); }
+        }
 
     }
 }
