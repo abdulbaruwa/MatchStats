596bab933e52a6813648b2832c058f259354614f
Shangoman <abdulbaruwa@hotmail.com>
2014-03-13 08:52:48 +0000

    Update UserProfileViewModel.cs

--- MatchStats/ViewModels/UserProfileViewModel.cs
+++ MatchStats/ViewModels/UserProfileViewModel.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Akavache;
+using MatchStats.Common;
+using MatchStats.Enums;
 using ReactiveUI;
 using MatchStats.Model;
 using System.Runtime.Serialization;
@@ -10,13 +13,13 @@ namespace MatchStats.ViewModels
     [DataContract]
     public class UserProfileViewModel : ReactiveObject
     {
-        public IReactiveCommand SaveDefaultPlayerCommand { get; protected set; }
+        public IReactiveCommand UpdateProfileCommand { get; protected set; }
         public IReactiveCommand NavAwayCommand { get; protected set; }
         private IBlobCache _blobCache;
         public UserProfileViewModel()
         {
-            SaveDefaultPlayerCommand = new ReactiveCommand(this.WhenAny(x => x.DefaultPlayer, p => PlayerIsValidForSave(p.Value)));
-            SaveDefaultPlayerCommand.Subscribe(_ => SaveDefaultPlayer());
+            UpdateProfileCommand = new ReactiveCommand(this.WhenAny(x => x.PlayerFirstName, x => x.PlayerSurname, x => x.SelectedPlayerRating, (firstname, surname, rating) => PlayerIsValidForSave(firstname.Value, surname.Value, rating.Value)));
+            UpdateProfileCommand.Subscribe(_ => SaveDefaultPlayer());
             NavAwayCommand = new ReactiveCommand();
             NavAwayCommand.Subscribe(_ => CloseSelf());
             _blobCache = RxApp.MutableResolver.GetService<IBlobCache>("UserAccount");
@@ -24,14 +27,19 @@ namespace MatchStats.ViewModels
                 .Subscribe(x => DefaultPlayer = x, ex => this.Log().ErrorException("Error getting DefaultPlayer", ex));
         }
 
-        private bool PlayerIsValidForSave(Player player)
+        private bool PlayerIsValidForSave(string firstname, string surname, object rating)
         {
-            if (player == null) return false;
-            return  !  (string.IsNullOrEmpty(player.FirstName) || string.IsNullOrEmpty(player.SurName) || string.IsNullOrEmpty(player.Rating));
+            return !string.IsNullOrEmpty(firstname) && !string.IsNullOrEmpty(surname) && rating != null;
         }
 
         private void SaveDefaultPlayer()
         {
+            DefaultPlayer = new Player()
+            {
+                FirstName = PlayerFirstName,
+                SurName = PlayerSurname,
+                Rating = SelectedPlayerRating.ToString()
+            };
             _blobCache.InsertObject("DefaultPlayer", DefaultPlayer);
         }
 
@@ -43,11 +51,6 @@ namespace MatchStats.ViewModels
             set { this.RaiseAndSetIfChanged(ref _defaultPlayer, value); }
         }
 
-        private void CloseSelf()
-        {
-            ShowMe = false;
-        }
-
         [DataMember]
         private bool _showMe;
         public bool ShowMe
@@ -55,5 +58,44 @@ namespace MatchStats.ViewModels
             get { return _showMe; }
             set { this.RaiseAndSetIfChanged(ref _showMe, value); }
         }
+
+        private static List<Rating> _ratings;
+        public List<Rating> Ratings
+        {
+            get { return _ratings ?? (_ratings = EnumHelper.GetEnumAsList<Rating>()); }
+        }
+
+
+        [DataMember]
+        private object _selectedPlayerRating;
+        public object SelectedPlayerRating
+        {
+            get { return _selectedPlayerRating; }
+            set { this.RaiseAndSetIfChanged(ref _selectedPlayerRating, value); }
+        }
+
+        [DataMember]
+        private string _playerFirstName;
+        public string PlayerFirstName
+        {
+            get { return _playerFirstName; }
+            set { this.RaiseAndSetIfChanged(ref _playerFirstName, value); }
+        }
+
+        [DataMember]
+        private string _playerSurname;
+        public string PlayerSurname
+        {
+            get { return _playerSurname; }
+            set { this.RaiseAndSetIfChanged(ref _playerSurname, value); }
+        }
+
+
+
+        private void CloseSelf()
+        {
+            ShowMe = false;
+        }
+
     }
 }
